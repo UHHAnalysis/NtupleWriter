69c9bee39343105eb0bc784061d474bc0ba8dea2
hoeing <>
2013-03-26 13:15:57 +0000

    adding tools and selection modules for tprime analysis

--- Objects/TopJet.h
+++ Objects/TopJet.h
@@ -12,17 +12,24 @@ class TopJet : public Jet{
  public:
   TopJet(){
     m_subjets.clear();
+    m_btagsub_combinedSecondaryVertex.clear();
+    m_flavorsub.clear();
   };
   ~TopJet(){
   };
 
   std::vector<Particle> subjets() const{return m_subjets;}
+  std::vector<float> btagsub_combinedSecondaryVertex() const{return m_btagsub_combinedSecondaryVertex;}
+  std::vector<int> flavorsub() const{return m_flavorsub;}
 
   void add_subjet(Particle p){m_subjets.push_back(p);}
+  void add_btagsub_combinedSecondaryVertex(float discr){m_btagsub_combinedSecondaryVertex.push_back(discr);}
+  void add_flavorsub(int flav){m_flavorsub.push_back(flav);}
 
  private:
   std::vector<Particle> m_subjets;
-
+  std::vector<float> m_btagsub_combinedSecondaryVertex;
+  std::vector<int> m_flavorsub;
 };
 
 #endif
